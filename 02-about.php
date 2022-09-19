1684cde97eb6dd001cf92191045d706d232a2a90
Manop Aemsiri <manop.aemsiri@gmail.com>
2022-09-19 17:05:43 +0700

    ๊๊Update css

--- 02-about.php
+++ 02-about.php
@@ -16,60 +16,74 @@
   <!-- come back to edit later -->
   <div class="tab-demo"></div>
 
-  <section data-section="2" class="section-padding" style="background-color:#f2f2f2">
+  <section data-section="2" class="section-01 section-padding" style="background-color:#f2f2f2">
     <div class="container">
       <div class="text-center">
-        <h1 class="title">About Us</h1> 
-        <p>บริษัท ซินเนอร์รี่ คอร์ปอเรชั่น (ประเทศไทย) จำกัด</p>  
-        <p>เราเป็นบริษัทที่ปรึกษา</p>
-        <p>“กลยุทธ์ด้านดิจิตัล สำหรับภาครัฐแบบครบวงจร”</p>
-        <p>เรานำเสนอโซลูชั่นผ่านการให้คำปรึกษาด้านกลยุทธ์ทางดิจิทัลและเทคโนโลยี</p>
-        <p>ที่ช่วยให้หน่วยงานหรือองค์กรได้ผลลัพธ์ที่ดีขึ้น เพื่อรับกับแนวทางการเปลี่ยนแปลงที่เกิดขึ้น</p>
+        <h1 class="h1 color-01 fw-600">About Us</h1> 
+        <p class="h6 color-01 fw-600">บริษัท ซินเนอร์รี่ คอร์ปอเรชั่น (ประเทศไทย) จำกัด</p>  
+        <p class="h5 sm fw-400">เราเป็นบริษัทที่ปรึกษา</p>
+        <p class="h2 color-01 fw-600">“กลยุทธ์ด้านดิจิตัล สำหรับภาครัฐแบบครบวงจร”</p>
+        <p class="fw-400">เรานำเสนอโซลูชั่นผ่านการให้คำปรึกษาด้านกลยุทธ์ทางดิจิทัลและเทคโนโลยี</p>
+        <p class="fw-400">ที่ช่วยให้หน่วยงานหรือองค์กรได้ผลลัพธ์ที่ดีขึ้น เพื่อรับกับแนวทางการเปลี่ยนแปลงที่เกิดขึ้น</p>
       </div>
     </div>
   </section>
 
-  <section data-section="3" class="section-padding" style="background-color:red;">
+  <section data-section="3" class="section-01 size-02 section-padding">
+    <div class="img-bg" style="background-image:url('public/img/bg/03.jpg');"></div>
     <div class="container">
-      <div class="text-center">
-        <p>ประสบการณ์ มากกว่า 25 ปี</p> 
-        <p>ความสำเร็จของลูกค้าของเรา ทั้งหน่วยงานภาครัฐและเอกชน มากกว่า 400 หน่วยงาน</p>  
-        <p>รวมผลงานต่างๆ มากกว่า 500 โครงการ</p>
+      <div class="text-center color-white">
+        <p class="lg">
+          <span class="fw-500 mr-1">ประสบการณ์</span>มากกว่า
+          <span class="fw-500">25 ปี</span></p> 
+        <p class="lg">
+          ความสำเร็จของ<span class="fw-500">ลูกค้าของเรา</span>
+          ทั้งหน่วยงานภาครัฐและเอกชน มากกว่า <span class="fw-500">400 หน่วยงาน</span>
+        </p>  
+        <p class="lg">
+          รวม<span class="fw-500">ผลงาน</span>ต่างๆ มากกว่า <span class="fw-500">500 โครงการ</span>
+        </p>
       </div>
-      <div class="ss-box">
+      <div class="ss-box mt-5">
         <div class="grids jc-center">
           <div class="grid lg-1-3">
             <div class="ss-card-01">
               <div class="text-container">
-                <p>More Than</p>
-                <p>300</p>
-                <p>Clients</p>
+                <p class="text-uppercase color-black fw-500">More Than</p>
+                <p class="h1 lg lh-2xs color-white fw-600">300</p>
+                <p class="h5 text-uppercase color-black fw-600">Clients</p>
               </div>
             </div>
           </div>
           <div class="grid lg-1-3">
             <div class="ss-card-01">
               <div class="text-container">
-                <p>More Than</p>
-                <p>300</p>
-                <p>Clients</p>
+                <p class="text-uppercase color-black fw-500">More Than</p>
+                <p class="h1 lg lh-2xs color-white fw-600">500</p>
+                <p class="h5 text-uppercase color-black fw-600">Clients</p>
               </div>
             </div>
           </div>
           <div class="grid lg-1-3">
             <div class="ss-card-01">
               <div class="text-container">
-                <p>More Than</p>
-                <p>300</p>
-                <p>Clients</p>
+                <p class="text-uppercase color-black fw-500">More Than</p>
+                <p class="h1 lg lh-2xs color-white fw-600">300</p>
+                <p class="h5 text-uppercase color-black fw-600">Clients</p>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="text-center mt-3">
-        <p>เรามุ่งเน้นการส่งมอบผลลัพธ์งานที่มี คุณภาพเหนือความคาดหมาย</p>
-        <p>ตรงตามกำหนด และ บรรลุผลการปฏิบัติงาน ให้กับลูกค้า</p>
+      <div class="text-center color-white mt-5">
+        <p class="lg">
+          เรามุ่งเน้นการส่งมอบผลลัพธ์งานที่มี 
+          <span class="fw-500">คุณภาพเหนือความคาดหมาย</span>
+        </p>
+        <p class="lg">
+          <span class="fw-500">ตรงตามกำหนด</span> และ 
+          <span class="fw-500">บรรลุผลการปฏิบัติงาน</span> ให้กับลูกค้า
+        </p>
       </div>
     </div>
   </section>
@@ -77,15 +91,16 @@
   <section data-section="4" class="section-padding" style="background-color:#f2f2f2">
     <div class="container">
       <div class="text-center">
-        <p class="title">“มาตรฐาน มืออาชีพ”</p>
-        <p class="desc sm-no-br">
+        <h4 class="color-01 fw-600">“มาตรฐาน มืออาชีพ”</h4>
+        <p class="sm-no-br color-black fw-400">
           บริษัทของเราประกอบด้วยผู้ที่ชอบการทำงานร่วมกัน ไม่ว่าทีมของเราจะอยู่ที่ใด เพื่อให้เราสามารถส่งมอบงานที่มีคุณภาพ <br>
           เราพัฒนาทักษะด้านกำลังคนขับเคลื่อนการปรับปรุงการปฏิบัติงาน และใช้วิธีการทำงานแบบใหม่ เพื่อเป้าหมายเดียวกัน <br>
-          คือการทำงานด้วย “มาตรฐานความ เป็นมืออาชีพ” หรือ Professional Standard จึงทำให้เราสามารถที่ <br>
-          จะส่งมอบงานได้ตามคำมั่นสัญญา ด้วยคุณภาพและมาตรฐานที่เหนือความคาดหมาย
+          คือการทำงานด้วย <span class="fw-600">“มาตรฐานความ เป็นมืออาชีพ”</span> หรือ 
+          <span class="fw-600">Professional Standard</span> จึงทำให้เราสามารถที่ <br>
+          จะส่งมอบงานได้ตามคำมั่นสัญญา ด้วย<span class="fw-600">คุณภาพและมาตรฐานที่เหนือความคาดหมาย</span>
         </p>
       </div>
-      <div class="ss-img horizontal no-hover mt-4">
+      <div class="ss-img horizontal no-hover mt-6">
         <div class="img-bg" style="background-image:url('public/img/content/01.jpg');"></div>
       </div>
     </div>
@@ -94,14 +109,15 @@
   <section data-section="5" class="section-padding">
     <div class="container">
       <div class="text-center">
-        <p class="title">Our Awards</p>
-        <p class="desc sm-no-br">
-        “Professional Standard” คือคำที่ทีมงานใช้ในการสื่อสารและยึดมั่นในการทำงานร่วมกัน ด้วยความมุ่งมั่นที่จะส่ง <br>
-        มอบ “งานคุณภาพเหนือ ความคาดหมาย” และ “ตรงต่อเวลา” ให้กับลูกค้า ทำให้หลายปีที่ผ่านมา เราได้รับทั้งคำ <br>
-        ชื่นชมและได้รับรางวัลต่าง ๆ มากมายเกี่ยวกับผลิตภัณฑ์ ผลงานและงานบริการที่บริษัทฯ ได้เป็นผู้พัฒนา และ ส่งมอบ
+        <h4 class="color-01 text-uppercase fw-600">Our Awards</h4>
+        <p class="sm-no-br color-black fw-400">
+          <span class="fw-600">“Professional Standard”</span> คือคำที่ทีมงานใช้ในการสื่อสารและยึดมั่นในการทำงานร่วมกัน 
+          ด้วยความมุ่งมั่นที่จะส่งมอบ <br> <span class="fw-600">“งานคุณภาพเหนือ ความคาดหมาย”</span> และ 
+          <span class="fw-600">“ตรงต่อเวลา”</span> ให้กับลูกค้า ทำให้หลายปีที่ผ่านมา เราได้รับทั้งคำ <br> ชื่นชมและได้รับรางวัลต่าง ๆ 
+          มากมายเกี่ยวกับผลิตภัณฑ์ ผลงานและงานบริการที่บริษัทฯ ได้เป็นผู้พัฒนา และ ส่งมอบงานคุณภาพให้กับลูกค้า
         </p>
       </div>
-      <div class="grids no-gap mt-4">
+      <div class="grids no-gap mt-6">
         <?php for($i=0; $i<6; $i++){?>
           <div class="grid lg-1-3 mt-0">
           <div class="ss-card ss-card-02">
@@ -113,6 +129,91 @@
         <?php }?>
       </div>
     </div>
+  </section>
+
+  <section data-section="6" class="section-01 section-padding">
+    <div class="img-bg" style="background-image:url('public/img/bg/01.jpg');"></div>
+    <div class="container">
+      <h4 class="text-center color-01 text-uppercase mb-4 fw-600">Why Us</h4>  
+      <div class="grids">
+        <div class="grid xl-50 lg-50 md-50 sm-50 xs-100">
+          <ul class="ss-list-01">
+          <li>	
+            มากกว่าการ “รับจ้าง” คือเรา “ทำงาน” เราช่วยลูกค้าคิด วางแผน วิเคราะห์   
+            เสมือนงานและเงินนั้นเป็นของทีมงานเราเอง 
+          </li>
+          <li>	
+            เราไม่ได้แค่ทำตามที่ลูกค้าต้องการ หรือสั่งให้ทำ แต่เราจะให้ความรู้ ให้คำ
+            ปรึกษาแนะนำ เพื่อพัฒนางานให้บรรลุเป้าหมายร่วมกัน 
+          </li>
+          <li>	
+            “Professional Standard” คือมาตรฐานในการทำงานร่วมกันของทีมงาน
+            เราและลูกค้าทุกราย 
+          </li>
+          <li>	
+            เราทำงาน ด้วยการกลับ รับประกัน KPI, ROI ที่ตกลงร่วมกันกับลูกค้า
+          </li>
+          </ul> 
+        </div>   
+        <div class="grid xl-50 lg-50 md-50 sm-50 xs-100">
+          <ul class="ss-list-01">
+            <li>	
+              เรามีเครื่องมือ และระบบโปรแกรม สำหรับใช้ในการบริหารจัดการ ระบบการ
+              ทำงานทุกจุด เพื่อคุณภาพและมาตรฐานระดับสากล
+            </li>
+            <li>	
+              เราใส่ใจในการคัดเลือกและพัฒนา ทีมงานของเราอย่างต่อเนื่องและปลูกฝัง
+              ความเป็นมืออาชีพในการทำงาน
+            </li>
+            <li>	
+              เรามีประสบการณ์ในการทำงาน Digital Business มานานกว่า 25 ปี
+              การันตีด้วยผลงานและลูกค้ากว่า 500 โครงการ 
+            </li>
+            <li>	
+              ทีมงานแบบจัดเต็ม ทุก Flow การทำงานตั้งแต่ต้นจนส่งมอบงาน  
+            </li>
+          </ul>
+        </div> 
+      </div>
+    </div>
+  </section>
+
+  <section data-section="7" class="section-01 size-01">
+    <div class="img-bg" style="background-image:url('public/img/bg/02.jpg');"></div>  
+    <div class="container">
+      <div class="btns d-flex jc-center">
+        <a class="btn btn-action style-01 btn-color-01" href="#">
+          สอบถามรายละเอียดเพิ่มเติม
+        </a>
+      </div>    
+    </div>  
+  </section>
+
+  <section data-section="8" style="background:#000000">
+    <h4 class="text-center color-01 fw-600">งานบริการอื่นๆ 
+      <span class="fw-100 color-white">ของเรา</span>
+    </h4>  
+    <div class="grids no-gap">
+      <?php for($i=0; $i<6; $i++){?>
+        <div class="grid xl-16">
+          <a class="ss-card ss-card-03 filter-01" href="#">
+            <div class="ss-img square">
+              <div class="img-bg" style="background-image:url('public/img/content/unsplash-0<?= ($i%6+1) ?>.jpg');"></div>
+            </div>
+            <div class="text-container">
+              <div class="title color-white" href="#">
+                Unlimited Microsite for Government
+                Unlimited Microsite for Government
+                Unlimited Microsite for Government
+              </div>
+            </div>
+            <div class="icon">
+              <i class="fa-solid fa-plus"></i>
+            </div>
+          </a>  
+        </div>   
+      <?php }?>   
+    </div>    
   </section>
   
   <?php include_once('include/footer.php'); ?>
