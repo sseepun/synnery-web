a732e99a7f5fdcdfe882445eee16f84562c0fa96
Manop Aemsiri <manop.aemsiri@gmail.com>
2022-09-29 18:35:34 +0700

    Update ERP for Government page

--- 09-erp-for-government.php
+++ 09-erp-for-government.php
@@ -13,6 +13,127 @@
     <img class="img" src="public/img/bg/47.jpg" alt="Banner" />
   </section>
 
+  <section data-section="2" class="section-01 section-padding">
+    <div class="container">
+      <h1 class="text-center color-01 lh-3xs fw-600" data-aos="fade-up" data-aos-delay="0">
+        ERP<span class="h3 color-black ml-1 mr-1 fw-300">or</span>E-Office
+      </h1>
+      <p class="h3 text-center lh-3xs" data-aos="fade-up" data-aos-delay="150">
+        for <span class="fw-600">Government Solutions</span>
+      </p>
+      <p class="h4 color-01 text-center mt-5 fw-600" data-aos="fade-up" data-aos-delay="300">
+        ถ้าปัญหาเหล่านี้คือสิ่งที่กำลังเกิดขึ้นในหน่วยงานของคุณ
+      </p>
+      <div class="ss-box xl mt-4" data-aos="fade-up" data-aos-delay="450">
+        <div class="grids">
+          <div class="grid xl-50 lg-50 md-50 sm-50 xs-100">
+            <ul class="ss-list-02 size-02">
+              <li class="pb-2">
+                <p class="color-black fw-600 mt-1">ระบบไม่รองรับเทคโนโลยีใหม่ๆ</p>
+                <p class="sm color-black fw-400">
+                  ไม่ว่าจะเป็นเรื่องของ Web Browser รุ่นใหม่ และหลากหลาย รวมถึงการทำงานบน Mobile/Tablet
+                </p>
+              </li>
+              <li class="pb-2">
+                <p class="color-black fw-600 mt-1">ระบบไม่รองรับเทคโนโลยีใหม่ๆ</p>
+                <p class="sm color-black fw-400">
+                  ไม่ว่าจะเป็นเรื่องของ Web Browser รุ่นใหม่ และหลากหลาย รวมถึงการทำงานบน Mobile/Tablet
+                </p>
+              </li>
+              <li class="pb-2">
+                <p class="color-black fw-600 mt-1">ระบบไม่รองรับเทคโนโลยีใหม่ๆ</p>
+                <p class="sm color-black fw-400">
+                  ไม่ว่าจะเป็นเรื่องของ Web Browser รุ่นใหม่ และหลากหลาย รวมถึงการทำงานบน Mobile/Tablet
+                </p>
+              </li>
+              <li class="pb-2">
+                <p class="color-black fw-600 mt-1">ระบบไม่รองรับเทคโนโลยีใหม่ๆ</p>
+                <p class="sm color-black fw-400">
+                  ไม่ว่าจะเป็นเรื่องของ Web Browser รุ่นใหม่ และหลากหลาย รวมถึงการทำงานบน Mobile/Tablet
+                </p>
+              </li>
+            </ul> 
+          </div>   
+          <div class="grid xl-50 lg-50 md-50 sm-50 xs-100">
+            <ul class="ss-list-02 size-02">
+              <li class="pb-2">
+                <p class="color-black fw-600 mt-1">ระบบไม่รองรับเทคโนโลยีใหม่ๆ</p>
+                <p class="sm color-black fw-400">
+                  ไม่ว่าจะเป็นเรื่องของ Web Browser รุ่นใหม่ และหลากหลาย รวมถึงการทำงานบน Mobile/Tablet
+                </p>
+              </li>
+              <li class="pb-2">
+                <p class="color-black fw-600 mt-1">ระบบไม่รองรับเทคโนโลยีใหม่ๆ</p>
+                <p class="sm color-black fw-400">
+                  ไม่ว่าจะเป็นเรื่องของ Web Browser รุ่นใหม่ และหลากหลาย รวมถึงการทำงานบน Mobile/Tablet
+                </p>
+              </li>
+              <li class="pb-2">
+                <p class="color-black fw-600 mt-1">ระบบไม่รองรับเทคโนโลยีใหม่ๆ</p>
+                <p class="sm color-black fw-400">
+                  ไม่ว่าจะเป็นเรื่องของ Web Browser รุ่นใหม่ และหลากหลาย รวมถึงการทำงานบน Mobile/Tablet
+                </p>
+              </li>
+              <li class="pb-2">
+                <p class="color-black fw-600 mt-1">ระบบไม่รองรับเทคโนโลยีใหม่ๆ</p>
+                <p class="sm color-black fw-400">
+                  ไม่ว่าจะเป็นเรื่องของ Web Browser รุ่นใหม่ และหลากหลาย รวมถึงการทำงานบน Mobile/Tablet
+                </p>
+              </li>
+            </ul>
+          </div> 
+        </div>  
+      </div> 
+    </div>
+  </section>
+
+  <section data-section="4" class="section-01 section-padding">
+    <div class="img-bg" style="background-image:url('public/img/bg/48.jpg');"></div>  
+    <div class="container">
+      <h4 class="text-center color-white fw-600" data-aos="fade-up" data-aos-delay="0">
+        Jigsaw ERP <span class="h5 fw-200">for Government &</span><br>Microservices Architechture
+      </h4>
+      <p class="sm text-center color-white fw-200" data-aos="fade-up" data-aos-delay="150">
+        ด้วยประสบการณ์ในการทำงานและแลกเปลี่ยนพูดคุย ถึงปัญหาเกี่ยวกับระบบเทคโนโลยีและสารสนเทศภาครัฐมานาน 
+        ทำให้เราเข้าใจและมองเห็นปัญหาที่ถูกสะสม<br>มายาวนาน ทำให้เราเข้าใจและสามารถออกแบบระบบใหม่ 
+        เพื่อแก้ไขปัญหาเก่าๆ และนำเทคโนโลยีใหม่ๆ เข้ามาใช้งาน 
+      </p>
+      <div class="ss-box xl mt-6" data-aos="fade-up" data-aos-delay="300">
+        <img class="img" src="public/img/content/05.png" alt="Banner" />
+      </div>
+    </div>
+  </section>
+
+  <section data-section="5" class="section-01 section-padding">
+    <div class="img-bg" style="background-image:url('public/img/bg/49.jpg');"></div>  
+    <div class="container">
+      <div>
+        <p class="h4 color-white text-center fw-100" data-aos="fade-up" data-aos-delay="0">
+          แล้วทำไมถึง
+        </p>
+        <p class="h4 text-center color-white fw-600" data-aos="fade-up" data-aos-delay="150">
+          ต้องปรับ ต้องเปลี่ยน
+        </p>
+      </div>
+      <div class="mt-5">
+        <p class="h4 color-white text-center fw-100" data-aos="fade-up" data-aos-delay="300">
+          เพราะ
+        </p>
+        <p class="h4 text-center color-white fw-600" data-aos="fade-up" data-aos-delay="450">
+          โลกของเทคโนโลยี ที่ไม่เคยหยุดนิ่ง
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <section data-section="6" class="section-01 section-padding">
+    <div class="img-bg" style="background-image:url('public/img/bg/50.jpg');"></div>  
+    <div class="container">
+      <h4 class="text-center">THE TREND OF <span class="color-01 fw-600">DEVELOPMENT</span></h4>
+    </div>
+  </section>
+  
+
   <section data-section="10" class="section-01 size-01">
     <div class="img-bg" style="background-image:url('public/img/bg/02.jpg');"></div>  
     <div class="container">
