9d803d5a7dc219c23732713038ba94098f01e4a9
Manop Aemsiri <manop.aemsiri@gmail.com>
2022-10-09 17:25:54 +0700

    Update script at footer

--- include/footer.php
+++ include/footer.php
@@ -162,3 +162,15 @@
     </filter>
   </defs>
 </svg>
+
+<script>
+    document.addEventListener("mousemove", parallax);
+    function parallax(event) {
+      this.querySelectorAll(".pattern .wrapper").forEach((shift) => {
+        const position = shift.getAttribute("value");
+        const x = (window.innerWidth - event.pageX * position) / 90;
+        const y = (window.innerHeight - event.pageY * position) / 90;
+        shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
+      });
+    }
+  </script>
