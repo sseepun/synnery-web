c3ce985da6bfd688fad85573723c30793bd5e3d5
zneb21 <nisakorn.benzzer@gmail.com>
2022-12-01 09:55:04 +0700

    Google tag

--- include/header.php
+++ include/header.php
@@ -11,3 +11,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-NZGTD78');</script>
 <!-- End Google Tag Manager -->
+
+<!-- Google tag (gtag.js) --> 
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-7D7NS61HBE"></script> 
+<script>   window.dataLayer = window.dataLayer || [];   function gtag(){dataLayer.push(arguments);}   gtag('js', new Date());   gtag('config', 'G-7D7NS61HBE'); </script>
