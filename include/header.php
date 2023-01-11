8add71bdde9a670f7dcc021d0c4f86dd24d9da71
zneb21 <nisakorn.benzzer@gmail.com>
2023-01-11 11:08:52 +0700

    Synerry title

--- include/header.php
+++ include/header.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<title>Synnery</title>
+<title>Synerry</title>
 <link rel="shortcut icon" type="image/x-icon" href="public/img/favicon.png" />
 
 <!-- Google Tag Manager -->
